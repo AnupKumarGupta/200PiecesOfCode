adf14c7e72125e9f4f7fe9cd6b3a8fc4ebd12c5e
Fyse Nassar <fyse.nassar@gmail.com>
2020-03-31 09:59:20 +0530

    Modified DP1

--- Fyse/Dynamic Programming/01_minimizing_coins.cpp
+++ Fyse/Dynamic Programming/01_minimizing_coins.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 int main()
 {
-    int coins[101], n, sum;
-    long long int value[1000001], minimum;
+    int coins[101], n, sum, ans;
+    long long int value[1000001];
     value[0] = 0;
     cin >> n >> sum;
 
     for (int i = 0; i < n; i++)
         cin >> coins[i];
-    minimum = coins[0];
-    for (int i = 0; i < n; i++)
-        if (minimum >= coins[i])
-            minimum = coins[i];
-
-    if (minimum > sum)
-    {
-        cout << "-1";
-        return 0;
-    }
+ 
 
     for (int i = 1; i <= sum; i++)
     {
@@ -29,10 +20,9 @@ int main()
             if (i - coins[j] >= 0)
                 value[i] = min(value[i], value[i - coins[j]] + 1);
     }
-    int ans;
-    if (value[sum] > sum)
-        ans = -1;
-    else
-        ans = value[sum];
+    if (value[sum] > sum) 
+         ans = -1;
+    else 
+         ans = value[sum];
     cout << ans;
 }
